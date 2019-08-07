39e1d08f5c7365b4f5dec92a49ee56b3c0156bc1
Matt <matthew.morley.ca@gmail.com>
2019-08-06 20:54:36 -0500

    implement falconmotor, falconencoder

---
+++ src/main/include/fl/motors/FalconEncoder.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <SIUnit.h>
+
+namespace fl {
+
+template <class T>
+class FalconEncoder {
+
+    /**
+     * The velocity of the encoder in [T]/s
+     */
+    double getVelocity();
+
+    /**
+     * The position of the encoder in [T]
+     */
+    double getPosition();
+
+    /**
+     * The velocity of the encoder in NativeUnits/s
+     */
+    double rawVelocity();
+    /**
+     * The position of the encoder in NativeUnits
+     */
+    double rawPosition();
+
+    fun resetPosition(newPosition: Double)
+
+};  
+} // namespace fl
--- src/main/include/fl/motors/FalconMotor.h
+++ src/main/include/fl/motors/FalconMotor.h
@@ -1,81 +1,83 @@
-// #pragma once
+#pragma once
 
-// #include <SIUnit.h>
+#include <SIUnit.h>
 
-// // namespace fl {
+namespace fl {
 
-// // template <SIUnit T>
-// // class FalconMotor<
+template <class T>
+class FalconMotor {
 
-// // };  // namespace fl
+    /**
+     * The encoder attached to the motor
+     */
+    FalconEncoder<T> encoder;
 
+    /**
+     * The voltage output of the motor controller in volts
+     */
+    double getVoltageOutput();
 
-// // // interface FalconMotor<T : SIUnit<T>> {
+    /**
+     * Inverts the output given to the motor
+     */
+    void setOutputInverted(bool isInverted);
 
-// //     /**
-// //      * The encoder attached to the motor
-// //      */
-// //     val encoder: FalconEncoder<T>
-// //     /**
-// //      * The voltage output of the motor controller in volts
-// //      */
-// //     val voltageOutput: Double
+    /**
+     *  When enabled, motor leads are commonized electrically to reduce motion
+     */
+    void setBrakeMode(bool isBrakeMode);
 
-// //     /**
-// //      * Inverts the output given to the motor
-// //      */
-// //     var outputInverted: Boolean
+    /**
+     * Configures the max voltage output given to the motor
+     */
+    void setVoltageCompensation(double compVoltage);
 
-// //     /**
-// //      *  When enabled, motor leads are commonized electrically to reduce motion
-// //      */
-// //     var brakeMode: Boolean
+    /**
+     *  Peak target velocity that the on board motion profile generator will use
+     *  Unit is [T]/s
+     */
+    void setMotionProfileVelocity(double newVelocity);
+    double getMotionProfileVelocity();
 
-// //     /**
-// //      * Configures the max voltage output given to the motor
-// //      */
-// //     var voltageCompSaturation: Double
+    /**
+     *  Acceleration that the on board motion profile generator will
+     *  Unit is [T]/s/s
+     */
+    void setMotionProfileAcceleration(double newAccel);
+    double getMotionProfileAcceleration();
 
-// //     /**
-// //      *  Peak target velocity that the on board motion profile generator will use
-// //      *  Unit is [T]/s
-// //      */
-// //     var motionProfileCruiseVelocity: Double
-// //     /**
-// //      *  Acceleration that the on board motion profile generator will
-// //      *  Unit is [T]/s/s
-// //      */
-// //     var motionProfileAcceleration: Double
-// //     /**
-// //      * Enables the use of on board motion profiling for position mode
-// //      */
-// //     var useMotionProfileForPosition: Boolean
+    /**
+     * Enables the use of on board motion profiling for position mode
+     */
+    void setUseMotionProfileForPosition(bool useMotionProfileForPosition);
+    bool getUseMotionProfileForPosition();
 
-// //     fun follow(motor: FalconMotor<*>): Boolean
+    bool follow(motor: FalconMotor);
 
-// //     /**
-// //      * Sets the output [voltage] in volts and [arbitraryFeedForward] in volts
-// //      */
-// //     fun setVoltage(voltage: Double, arbitraryFeedForward: Double = 0.0)
+    /**
+     * Sets the output [voltage] in volts and [arbitraryFeedForward] in volts
+     */
+    void setVoltage(double voltage, double arbitraryFeedForward = 0.0);
 
-// //     /**
-// //      * Sets the output [dutyCycle] in percent and [arbitraryFeedForward] in volts
-// //      */
-// //     fun setDutyCycle(dutyCycle: Double, arbitraryFeedForward: Double = 0.0)
+    /**
+     * Sets the output [dutyCycle] in percent and [arbitraryFeedForward] in volts
+     */
+    void setDutyCycle(double dutyCycle, double arbitraryFeedForward = 0.0);
 
-// //     /**
-// //      * Sets the output [velocity] in [T]/s and [arbitraryFeedForward] in volts
-// //      */
-// //     fun setVelocity(velocity: Double, arbitraryFeedForward: Double = 0.0)
+    /**
+     * Sets the output [velocity] in [T]/s and [arbitraryFeedForward] in volts
+     */
+    void setVelocity(double velocity, double arbitraryFeedForward = 0.0);
 
-// //     /**
-// //      * Sets the output [position] in [T] and [arbitraryFeedForward] in volts
-// //      */
-// //     fun setPosition(position: Double, arbitraryFeedForward: Double = 0.0)
+    /**
+     * Sets the output [position] in [T] and [arbitraryFeedForward] in volts
+     */
+    void setPosition(double position, double arbitraryFeedForward = 0.0);
 
-// //     /**
-// //      * Sets the output of the motor to neutral
-// //      */
-// //     fun setNeutral()
+    /**
+     * Sets the output of the motor to neutral
+     */
+    void setNeutral();
 
-// // }
+};
+}  // namespace fl
